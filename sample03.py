456da04341d09d18d8e5eef8ab60448455322232
se-lina <36369878+se-lina@users.noreply.github.com>
2023-07-07 23:39:34 +0900

    Create sample03.py

---
+++ sample03.py
@@ -0,0 +1,37 @@
+#フリップさせる動作
+
+from djitellopy import Tello  
+import time  
+  
+# Telloオブジェクトを作成  
+tello = Tello()  
+  
+# Telloに接続  
+tello.connect()  
+  
+# Telloを離陸させる  
+tello.takeoff()  
+  
+# 右にフリップ  
+tello.flip("r")  
+  
+# 左にフリップ  
+tello.flip("l")  
+  
+# 前進1m  
+tello.move_forward(100)  
+  
+# 右に移動1m  
+tello.move_right(100)  
+  
+# 左に移動1m  
+tello.move_left(100)  
+  
+# 下降1m  
+tello.move_down(100)  
+  
+# Telloを着陸させる  
+tello.land()  
+  
+# Telloとの接続を解除  
+tello.end()  
